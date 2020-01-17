9003a9c8b7d13ba009aab4dd9422a12d93ab31b8
vraptor <emil.korladinov@gmail.com>
2020-01-17 22:39:44 +0100

    Initial

---
+++ src/main/java/co/jware/examples/multitenant/LiquibaseMultiTenantApplication.java
@@ -0,0 +1,13 @@
+package co.jware.examples.multitenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseMultiTenantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseMultiTenantApplication.class, args);
+    }
+
+}
